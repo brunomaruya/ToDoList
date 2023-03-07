d67ba7fe25b28818ab01a95210651a5bbbd784d6
Bruno <brun0yuj1ym02@gmail.com>
2023-03-07 21:51:52 +0900

    create addTask and handleOnChange functions

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import { TasksContext } from '@/context/TasksContext';
+import React, { useContext } from 'react';
 
 export default function Input() {
+  const { addTask, handleOnChange } = useContext(TasksContext);
   return (
     <div className="flex gap-2 shadow-xl">
       <input
         className="bg-gray-800 rounded-lg py-2 px-4 w-full text-white"
         placeholder="Add a New Task"
         type="text"
+        onChange={handleOnChange}
       />{' '}
-      <button className="bg-blue-800 text-white py-2 px-4 rounded-lg hover:brightness-75 active:brightness-50">
+      <button
+        className="bg-blue-800 text-white py-2 px-4 rounded-lg hover:brightness-75 active:brightness-50"
+        onClick={addTask}
+      >
         Add
       </button>
     </div>
--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -3,14 +3,27 @@ import React, { createContext, ReactNode, useState } from 'react';
 
 type TasksContextType = {
   tasks: string[];
+  addTask: () => void;
+  task: string;
+  setTask: any;
+  handleOnChange: () => void;
 };
 
 export const TasksContext = createContext({} as TasksContextType);
 
 export const TasksProvider = ({ children }: { children: ReactNode }) => {
   const [tasks, setTasks] = useState<string[]>([]);
+  const [task, setTask] = useState<string>('');
 
-  const values = { tasks, setTasks };
+  const handleOnChange = (event) => {
+    setTask(event.target.value);
+  };
+  const addTask = () => {
+    setTasks([...tasks, task]);
+    setTask('');
+  };
+
+  const values = { tasks, setTasks, addTask, task, setTask, handleOnChange };
   return (
     <TasksContext.Provider value={values}>{children}</TasksContext.Provider>
   );
