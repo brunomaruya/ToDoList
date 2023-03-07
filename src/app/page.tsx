5c15050531fd4aa69f202d6014b9a628475c3671
Bruno <brun0yuj1ym02@gmail.com>
2023-03-07 09:43:04 +0900

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>test</h1>
     </main>
   );
 }
